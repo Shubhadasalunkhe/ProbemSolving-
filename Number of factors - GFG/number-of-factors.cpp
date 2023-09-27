8e9414e86dec6bd65f32b8449ff8c6a9584d7b54
Shubhadasalunkhe <121441310+Shubhadasalunkhe@users.noreply.github.com>
2023-09-27 17:42:02 +0530

    Added solution - LeetHub

---
+++ Number of factors - GFG/number-of-factors.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int countFactors(int N){
+        //code here
+              int count=2;
+        
+        for(int i=2; i<=sqrt(N) ; i++ )
+         {
+             if(N%i==0)
+              {
+                  count++;
+               if(i != N/i) count++;   
+              }
+                  
+         }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.countFactors(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
