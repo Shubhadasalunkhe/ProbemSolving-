5c5f9243682ce5fa7147a007e21e5218145ae2cc
Shubhadasalunkhe <121441310+Shubhadasalunkhe@users.noreply.github.com>
2023-10-23 23:53:45 +0530

    Added solution - LeetHub

---
+++ Counting Sort - GFG/counting-sort.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+#define RANGE 255
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    //Function to arrange all letters of a string in lexicographical 
+    //order using Counting Sort.
+    string countSort(string arr){
+        // code here
+        vector<int>alphaFreq(26);
+        for(auto x : arr){
+            alphaFreq[x-'a']++;
+        }
+        string ans;
+        for(int i=0;i<26;i++){
+            char c = i+'a';
+            while(alphaFreq[i]--){
+                ans+=c;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr;
+        cin>>arr;
+        Solution obj;
+        cout<<obj.countSort(arr)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
